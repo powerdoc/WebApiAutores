295b34b100023952af599336adf6879a65b16d19
Werdoc <powerdoc.werdoc@googlemail.com>
2022-09-15 01:11:22 -0500

    Adding the user registration logic

---
+++ WebApiAutores/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApiAutores.DTOs;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public AccountController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("register")] // api/accounts/register
+        public async Task<ActionResult<AuthenticationResponse>> Register(UserCredentials userCredentials)
+        {
+            var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email };
+            var result = await userManager.CreateAsync(user, userCredentials.Password);
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        private AuthenticationResponse BuildToken(UserCredentials userCredentials)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", userCredentials.Email),
+                new Claim("lo que yo quiera", "cualquier otro valor")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["jwtkey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims = claims, expires: expiration, signingCredentials: creds);
+
+            return new AuthenticationResponse()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+                Expiration = expiration
+            };
+        }
+    }
+}
